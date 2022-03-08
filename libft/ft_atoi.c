5a10593ab03edce05d656cd44e3adfc205ef9645
lbricio <kirowhisky@gmail.com>
2022-03-08 11:01:08 -0300

    urauraura

---
+++ libft/ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbricio- <lbricio-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/24 01:31:24 by lbricio-          #+#    #+#             */
+/*   Updated: 2021/06/07 13:03:14 by lbricio-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	ft_isspace(int c)
+{
+	if (c == ' ' || c == '\n' || c == '\t' || c == '\r'
+		|| c == '\f' || c == '\v')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *nptr)
+{
+	int	num;
+	int	sign;
+
+	while ((ft_isspace(*nptr)))
+		nptr++;
+	sign = 1;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	num = 0;
+	while ((ft_isdigit(*nptr)))
+	{
+		num *= 10;
+		num += sign * (*nptr - '0');
+		nptr++;
+	}
+	return (num);
+}
